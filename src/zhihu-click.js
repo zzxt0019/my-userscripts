321018999699c52328a7c31e3497703a429aeb5a
lxp <hello_xp@outlook.com>
2023-01-11 10:48:27 +0800

    init

---
+++ src/zhihu-click.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         知乎关闭 自动前往活动
+// @version      0.2
+// @description  自动关闭知乎首页的"自动前往活动"的框
+// @author       zzxt0019
+// @match        https://www.zhihu.com/
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    // Your code here...
+    let svgs = document.querySelectorAll('.Topstory > :not(.Topstory-container) svg');
+    let event = new Event('click', {bubbles: true, cancelable: false})
+    for (let i = 0; i < svgs.length; i++) {
+        svgs[i].dispatchEvent(event);
+    }
+})();
