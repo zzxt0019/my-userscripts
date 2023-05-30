2c057b1b5a293198552016ebbc9e94a44f82902b
lxp <hello_xp@outlook.com>
2023-05-30 15:22:42 +0800

    支持search页面屏蔽盐选

--- src/zhihu-yanxuan.js
+++ src/zhihu-yanxuan.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         知乎首页关闭盐选
-// @version      0.2
+// @version      0.3
 // @description  知乎首页关闭盐选 点开如果有盐选标识就不显示
 // @author       zzxt0019
-// @match        https://www.zhihu.com/
+// @match        https://www.zhihu.com/*
 // @grant        none
 // ==/UserScript==
 
@@ -27,7 +27,10 @@
     observer.observe(document, {childList: true, subtree: true, attributes: true});
 
     function init() {
-        let articles = document.querySelectorAll(`div.Card.TopstoryItem:not([${UserscriptId}])`);
+        let articles = document.querySelectorAll([
+            `div.Card.TopstoryItem:not([${UserscriptId}])`,  // 首页
+            `div.Card.SearchResult-Card:not([${UserscriptId}])`,  // 搜索页
+        ].join(','));
         for (let i = 0; i < articles.length; i++) {
             let article = articles[i];
             article.setAttribute(UserscriptId, '');
