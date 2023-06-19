9268e05c06e83ca308c3d3f97396f04acd3e8df9
zzxt0019 <60280520+zzxt0019@users.noreply.github.com>
2023-06-19 02:53:31 +0000

    Deploying to gh-pages from @ zzxt0019/my-userscripts@63f63be33327291932c77df383c5a108260a129b 🚀

--- hide-img.js
+++ hide-img.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         隐藏图片
-// @version      0.4
+// @version      0.5
 // @description  隐藏图片, 旁边添加按钮可展示/隐藏
 // @author       zzxt0019
 // @match        https://nga.178.com/*
@@ -13,6 +13,7 @@
     const UserscriptImgId = 'zzxt0019-hide-img';
     const UserscriptButtonId = 'zzxt0019-hide-button';
     const DisplayKey = 'zzxt0019-hide-display';
+    const NoneDisplayKey = 'zzxt0019-hide-none-display';
 
     let styles = {
         buttonSvgToSee: '<svg viewBox="64 64 896 896" focusable="false" data-icon="eye" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M81.8 537.8a60.3 60.3 0 010-51.5C176.6 286.5 319.8 186 512 186c-192.2 0-335.4 100.5-430.2 300.3a60.3 60.3 0 000 51.5C176.6 737.5 319.9 838 512 838c-192.1 0-335.4-100.5-430.2-300.2z" fill="#e6f7ff"></path><path d="M512 258c-161.3 0-279.4 81.8-362.7 254C232.6 684.2 350.7 766 512 766c161.4 0 279.5-81.8 362.7-254C791.4 339.8 673.3 258 512 258zm-4 430c-97.2 0-176-78.8-176-176s78.8-176 176-176 176 78.8 176 176-78.8 176-176 176z" fill="#e6f7ff"></path><path d="M942.2 486.2C847.4 286.5 704.1 186 512 186c-192.2 0-335.4 100.5-430.2 300.3a60.3 60.3 0 000 51.5C176.6 737.5 319.9 838 512 838c192.2 0 335.4-100.5 430.2-300.3 7.7-16.2 7.7-35 0-51.5zM512 766c-161.3 0-279.4-81.8-362.7-254C232.6 339.8 350.7 258 512 258s279.4 81.8 362.7 254C791.5 684.2 673.4 766 512 766z" fill="#1890ff"></path><path d="M508 336c-97.2 0-176 78.8-176 176s78.8 176 176 176 176-78.8 176-176-78.8-176-176-176zm0 288c-61.9 0-112-50.1-112-112s50.1-112 112-112 112 50.1 112 112-50.1 112-112 112z" fill="#1890ff"></path></svg>',
@@ -33,6 +34,9 @@
     style.innerHTML = `
     .${DisplayKey} {
       display: none !important;
+    }
+    .${NoneDisplayKey} {
+      opacity: 0.3 !important;
     }`;
     document.body.append(style);
 
@@ -60,10 +64,12 @@
                         button.setAttribute(DisplayKey, '');
                         button.innerHTML = styles.buttonSvgToHide;
                         document.querySelector(`[${UserscriptImgId}="${count0}"]`).classList.remove(DisplayKey);
+                        document.querySelector(`[${UserscriptImgId}="${count0}"]`).classList.add(NoneDisplayKey);
                     } else {
                         button.setAttribute(DisplayKey, 'none');
                         button.innerHTML = styles.buttonSvgToSee;
                         document.querySelector(`[${UserscriptImgId}="${count0}"]`).classList.add(DisplayKey);
+                        document.querySelector(`[${UserscriptImgId}="${count0}"]`).classList.remove(NoneDisplayKey);
                     }
                 };
                 img.parentElement.insertBefore(button, img);
