0f2a69ad7aee9550695354a4512f7cf140c352aa
zzxt0019 <60280520+zzxt0019@users.noreply.github.com>
2024-03-04 18:37:36 +0000

    Deploying to gh-pages from @ zzxt0019/my-userscripts@e4282e1567f2521de8716e37a08f6fb760322d8c 🚀

--- bilibili.js
+++ bilibili.js
@@ -1,31 +1,41 @@
 // ==UserScript==
-// @name         zzxt0019-bilibili a标签
+// @name         zzxt0019-bilibili
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  不能右键+T在新标签页打开链接?
+// @description  bilibili脚本
 // @author       You
-// @match        https://www.bilibili.com/*
+// @match        http*://*bilibili*/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
-    setInterval(()=>{
-        dataUrl();
-        dataUserId();
-    },1000)
-    function dataUrl(){
+    setInterval(() => {
+        dataUrl();  // a标签右键补充href(可以右键打开新页面)
+        dataUserId();  // a标签右键补充href(可以右键打开新页面)
+        liveNoLeave();  // 直播页长时间未操作提示(自动点击提示)
+    }, 1000)
+
+    function dataUrl() {
         let list = document.querySelectorAll('[data-url]:not([href])');
-        for(let i = 0;i<list.length;i++){
+        for (let i = 0; i < list.length; i++) {
             let element = list[i];
-            element.setAttribute('href',element.getAttribute('data-url'));
+            element.setAttribute('href', element.getAttribute('data-url'));
         }
     }
-    function dataUserId(){
+
+    function dataUserId() {
         let list = document.querySelectorAll('[data-user-id]:not([href])');
-        for(let i=0;i<list.length;i++){
+        for (let i = 0; i < list.length; i++) {
             let element = list[i];
-            element.setAttribute('href','https://space.bilibili.com/'+element.getAttribute('data-user-id'));
+            element.setAttribute('href', 'https://space.bilibili.com/' + element.getAttribute('data-user-id'));
+        }
+    }
+
+    function liveNoLeave() {
+        if (document.querySelector('div.prompt button')) {
+            let event = new Event('click', {bubbles: true, cancelable: false});
+            document.querySelector('div.prompt button').dispatchEvent(event);
         }
     }
 })();
